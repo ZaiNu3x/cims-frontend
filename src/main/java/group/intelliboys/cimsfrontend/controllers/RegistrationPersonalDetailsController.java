0c83052b86fa2b5b2885500881a7f6e2d3c210c8
ZaiNu3x <zainu3x007@gmail.com>
2024-05-23 10:19:28 -0700

    12th commit

--- src/main/java/group/intelliboys/cimsfrontend/controllers/RegistrationPersonalDetailsController.java
+++ src/main/java/group/intelliboys/cimsfrontend/controllers/RegistrationPersonalDetailsController.java
@@ -14,8 +14,11 @@ import javafx.scene.layout.Pane;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.List;
 import java.util.Objects;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegistrationPersonalDetailsController implements Initializable {
 
@@ -81,15 +84,49 @@ public class RegistrationPersonalDetailsController implements Initializable {
     private ImageView emailStatus;
     // ===========================================================
 
+    // ======================== HOUSE NUMBER =========================
+    @FXML
+    private Pane houseNumPane;
+
+    @FXML
+    private TextField houseNumField;
+
+    @FXML
+    private ImageView houseNumStatus;
+    // ===========================================================
+
+    // ======================== CITY =========================
+    @FXML
+    private Pane cityPane;
+
+    @FXML
+    private ComboBox<String> cityField;
+
+    // ===========================================================
+
+    // ======================== BARANGAY =========================
+    @FXML
+    private Pane barangayPane;
+
+    @FXML
+    private ComboBox<String> barangayField;
+
+    // ===========================================================
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         genderField.getItems().addAll("Male", "Female");
+        cityField.getItems().addAll("NCR - Taguig City", "NCR - Makati City");
     }
 
     public boolean isLastnameValid() {
         String lastname = lastnameField.getText();
 
-        if (lastname.length() < 8 || lastname.length() > 30 || lastname.contains(" ")) {
+        Pattern pattern = Pattern.compile("[^a-zA-Z0-9\\\\s]");
+        Matcher matcher = pattern.matcher(lastname);
+
+        if (lastname.length() < 3 || lastname.length() > 30 || matcher.find()) {
             lastnamePane.setStyle("-fx-border-width: 2; -fx-border-color: red;");
             lastnameStatus.setImage(new Image(Objects.requireNonNull(App.class.getResource("images/invalid.png")).toString()));
             lastnameField.setTooltip(new Tooltip("Invalid Lastname!"));
@@ -103,9 +140,12 @@ public class RegistrationPersonalDetailsController implements Initializable {
     }
 
     public boolean isFirstnameValid() {
-        String lastname = firstnameField.getText();
+        String firstname = firstnameField.getText();
 
-        if (lastname.length() < 8 || lastname.length() > 30 || lastname.contains(" ")) {
+        Pattern pattern = Pattern.compile("[^a-zA-Z0-9\\\\s]");
+        Matcher matcher = pattern.matcher(firstname);
+
+        if (firstname.length() < 3 || firstname.length() > 30 || matcher.find()) {
             firstnamePane.setStyle("-fx-border-width: 2; -fx-border-color: red;");
             firstnameStatus.setImage(new Image(Objects.requireNonNull(App.class.getResource("images/invalid.png")).toString()));
             firstnameField.setTooltip(new Tooltip("Invalid Firstname!"));
@@ -119,9 +159,12 @@ public class RegistrationPersonalDetailsController implements Initializable {
     }
 
     public boolean isMiddlenameValid() {
-        String lastname = middlenameField.getText();
+        String firstname = middlenameField.getText();
 
-        if (lastname.length() < 8 || lastname.length() > 30 || lastname.contains(" ")) {
+        Pattern pattern = Pattern.compile("[^a-zA-Z0-9\\\\s]");
+        Matcher matcher = pattern.matcher(firstname);
+
+        if (firstname.length() < 3 || firstname.length() > 30 || matcher.find()) {
             middlenamePane.setStyle("-fx-border-width: 2; -fx-border-color: red;");
             middlenameStatus.setImage(new Image(Objects.requireNonNull(App.class.getResource("images/invalid.png")).toString()));
             middlenameField.setTooltip(new Tooltip("Invalid Middlename!"));
@@ -137,7 +180,7 @@ public class RegistrationPersonalDetailsController implements Initializable {
     public boolean isGenderValid() {
         String gender = genderField.getValue();
 
-        if (gender.isEmpty()) {
+        if (gender.length() < 4) {
             genderPane.setStyle("-fx-border-width: 2; -fx-border-color: red;");
             genderField.setTooltip(new Tooltip("Invalid Gender!"));
             return false;
@@ -173,8 +216,148 @@ public class RegistrationPersonalDetailsController implements Initializable {
         } else {
             emailPane.setStyle("-fx-border-width: 2; -fx-border-color: red;");
             emailStatus.setImage(new Image(Objects.requireNonNull(App.class.getResource("images/invalid.png")).toString()));
-            emailField.setTooltip(new Tooltip("Invalid Emmail!"));
+            emailField.setTooltip(new Tooltip("Invalid Email!"));
             return true;
+        }
+    }
+
+    public boolean isHouseNumValid() {
+        String houseNum = houseNumField.getText();
+
+        if (houseNum.length() < 3 || houseNum.length() > 127) {
+            houseNumPane.setStyle("-fx-border-width: 2; -fx-border-color: red;");
+            houseNumStatus.setImage(new Image(Objects.requireNonNull(App.class.getResource("images/invalid.png")).toString()));
+            houseNumField.setTooltip(new Tooltip("Invalid House Number!"));
+            return false;
+        } else {
+            houseNumPane.setStyle("-fx-border-width: 2; -fx-border-color: rgb(36, 76, 230);");
+            houseNumStatus.setImage(new Image(Objects.requireNonNull(App.class.getResource("images/check.png")).toString()));
+            houseNumField.setTooltip(null);
+            return true;
+        }
+    }
+
+    public boolean isCityValid() {
+        String city = cityField.getValue();
+
+        if (city.isEmpty()) {
+            cityPane.setStyle("-fx-border-width: 2; -fx-border-color: red;");
+            cityField.setTooltip(new Tooltip("Invalid City!"));
+            return false;
+        } else {
+            cityPane.setStyle("-fx-border-width: 2; -fx-border-color: rgb(36, 76, 230);");
+            cityField.setTooltip(null);
+            return true;
+        }
+    }
+
+    public boolean isBarangayValid() {
+        String barangay = barangayField.getValue();
+
+        if (barangay == null) {
+            barangayPane.setStyle("-fx-border-width: 2; -fx-border-color: red;");
+            barangayField.setTooltip(new Tooltip("Invalid Barangay!"));
+            return false;
+        } else {
+            barangayPane.setStyle("-fx-border-width: 2; -fx-border-color: rgb(36, 76, 230);");
+            barangayField.setTooltip(null);
+            return true;
+        }
+    }
+
+    public void cityChangedValue() {
+
+        if (isCityValid()) {
+            String city = cityField.getValue();
+
+            if (city.equals("NCR - Taguig City")) {
+
+                barangayField.getItems().clear();
+
+                List<String> barangays = List.of("Bagumbayan",
+                        "Bambang",
+                        "Calzada",
+                        "Central Bicutan",
+                        "Central Signal Village",
+                        "Fort Bonifacio",
+                        "Hagonoy",
+                        "Ibayo-Tipas",
+                        "Katuparan",
+                        "Ligid-Tipas",
+                        "Lower Bicutan",
+                        "Maharlika Village",
+                        "Napindan",
+                        "New Lower Bicutan",
+                        "North Daang Hari",
+                        "North Signal Village",
+                        "Palingon",
+                        "Pinagsama",
+                        "San Miguel",
+                        "Santa Ana",
+                        "Tuktukan",
+                        "Upper Bicutan",
+                        "Ususan",
+                        "Wawa",
+                        "Western Bicutan");
+
+                barangayField.getItems().addAll(barangays);
+
+            } else if (city.equals("NCR - Makati City")) {
+
+                barangayField.getItems().clear();
+
+                List<String> barangays = List.of("Bangkal",
+                        "Bel-Air",
+                        "Carmona",
+                        "Cembo",
+                        "Comembo",
+                        "Dasmarinas",
+                        "East Rembo",
+                        "Forbes Park",
+                        "Guadalupe Nuevo",
+                        "Guadalupe Viejo",
+                        "Kasilawan",
+                        "La Paz",
+                        "Magallanes",
+                        "Olympia",
+                        "Palanan",
+                        "Pembo",
+                        "Pinagkaisahan",
+                        "Pio del Pilar",
+                        "Pitogo",
+                        "Poblacion",
+                        "Post Proper Northside",
+                        "Post Proper Southside",
+                        "Rizal",
+                        "San Antonio",
+                        "San Isidro",
+                        "San Lorenzo",
+                        "Santa Cruz",
+                        "Singkamas",
+                        "South Cembo",
+                        "Tejeros",
+                        "Urdaneta",
+                        "Valenzuela");
+
+                barangayField.getItems().addAll(barangays);
+            }
+        }
+    }
+
+    public void barangayChangedValue() {
+        isBarangayValid();
+    }
+
+    private boolean isFormValid() {
+        return isLastnameValid() && isFirstnameValid() && isMiddlenameValid() && isGenderValid()
+                && isBirthDateValid() && isEmailValid() && isHouseNumValid() && isCityValid() && isBarangayValid();
+    }
+
+    public void nextButtonClicked() {
+        if (isFormValid()) {
+            System.out.println("Valid!");
+        } else {
+            System.out.println("Invalid!");
         }
     }
 }
